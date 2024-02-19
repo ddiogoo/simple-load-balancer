b06b57efc278e276415a6c51c13cc57b5ab262ac
diogomsz <diogomrtsa@gmail.com>
2024-02-19 17:09:54 -0300

    Implementando um Channel

---
+++ src/main/java/channel/Channel.java
@@ -0,0 +1,31 @@
+package channel;
+
+import worker.Worker;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class Channel {
+    private final int numWorkers;
+    private final int numTasks;
+    private BlockingQueue<Integer> channel;
+
+    public Channel(int numWorkers, int numTasks) {
+        this.numWorkers = numWorkers;
+        this.numTasks = numTasks;
+        channel = new ArrayBlockingQueue<>(numWorkers);
+    }
+
+    public void run() {
+        for(int i = 0; i < numWorkers; i++) {
+            new Thread(new Worker(i, channel)).start();
+        }
+        for(int i = 0; i < numTasks; i++) {
+            try {
+                channel.put(i);
+            } catch(InterruptedException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+    }
+}
